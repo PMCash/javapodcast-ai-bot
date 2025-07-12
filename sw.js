c4e98e03ac062199327e697095222841ecc6b3ca
Philip Chinedu Nwakusor <161179134+PMCash@users.noreply.github.com>
2025-07-12 14:58:35 +0100

    Improve service worker caching

--- sw.js
+++ sw.js
@@ -1,7 +1,30 @@
-self.addEventListener('install', function(event) {
-  console.log('Service Worker installed');
+const CACHE_NAME = 'javapodcast-cache-v1';
+const OFFLINE_URL = 'index.html';
+const ASSETS_TO_CACHE = [
+  '/',
+  'index.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).catch(() => {
+        if (event.request.mode === 'navigate') {
+          return caches.match(OFFLINE_URL);
+        }
+      });
+    })
+  );
 });
